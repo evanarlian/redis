1fc577d201c06fc2b40e80776f2474735d70a323
Evan Arlian <evanarlian2000@gmail.com>
2024-01-30 13:50:42 +0700

    write PONG

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +7,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write_all("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
