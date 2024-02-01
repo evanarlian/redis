6b03a8aa1ec616d4c5b206e175fdd385e21de83b
Evan Arlian <evanarlian2000@gmail.com>
2024-02-01 13:36:04 +0700

    WHYYYYYYYYYYYYY

--- src/resp/dtypes.rs
+++ src/resp/dtypes.rs
@@ -1,15 +1,16 @@
 // https://redis.io/docs/reference/protocol-spec
 
+// Also bad naming tbh
 trait RespIO {
+    const FIRST_BYTE: char;
     fn to_resp(buf: Vec<&str>) -> Result<Resp, &str>;
     fn to_output(&self) -> String;
 }
-enum Resp<'a> {
+pub enum Resp<'a> {
     SimpleString(SimpleString<'a>),
     SimpleError(SimpleError<'a>),
     Integer(Integer),
     BulkString(BulkString<'a>),
-    Array(Array<'a>),
     Null(Null),
     Boolean(Boolean),
     Double(Double),
@@ -22,7 +23,6 @@ impl<'a> Resp<'a> {
             Resp::SimpleError(_) => '-',
             Resp::Integer(_) => ':',
             Resp::BulkString(_) => '$',
-            Resp::Array(_) => '*',
             Resp::Null(_) => '_',
             Resp::Boolean(_) => '#',
             Resp::Double(_) => ',',
@@ -43,7 +43,6 @@ impl<'a> RespIO for Resp<'a> {
             '-' => SimpleError::to_resp(buf),
             ':' => Integer::to_resp(buf),
             '$' => BulkString::to_resp(buf),
-            '*' => Array::to_resp(buf),
             '_' => Null::to_resp(buf),
             '#' => Boolean::to_resp(buf),
             ',' => Double::to_resp(buf),
@@ -57,7 +56,6 @@ impl<'a> RespIO for Resp<'a> {
             Resp::SimpleError(_) => todo!(),
             Resp::Integer(_) => todo!(),
             Resp::BulkString(_) => todo!(),
-            Resp::Array(_) => todo!(),
             Resp::Null(_) => todo!(),
             Resp::Boolean(_) => todo!(),
             Resp::Double(_) => todo!(),
@@ -65,6 +63,9 @@ impl<'a> RespIO for Resp<'a> {
         }
     }
 }
+
+
+
 
 struct SimpleString<'a>(&'a str);
 impl<'a> RespIO for SimpleString<'a> {
@@ -99,7 +100,7 @@ impl RespIO for Integer {
 
 
 // TODO turns out implementation of Bulk String is just String
-pub struct BulkString<'a>(Vec<&'a str>);
+pub struct BulkString<'a>(pub &'a str);
 impl<'a> RespIO for BulkString<'a> {
     fn to_resp(buf: Vec<&str>) -> Result<Resp, &str> {
         todo!()
@@ -130,46 +131,7 @@ impl<'a> BulkString<'a> {
     }
 }
 
-pub struct Array<'a>(Vec<&'a str>);
-impl<'a> RespIO for Array<'a> {
-    fn to_resp(buf: Vec<&str>) -> Result<Resp, &str> {
-        todo!()
-    }
-    fn to_output(&self) -> String {
-        todo!()
-    }
-}
 
-impl<'a> Array<'a> {
-    // TODO move to standalone function
-    pub fn from_client_bytes(buffer: &[u8]) -> Result<Array, &'static str> {
-        // wild assumption: all inputs must be valid string
-        let splitted = std::str::from_utf8(buffer)
-            .map_err(|_| "not a valid utf-8 string")?
-            .split("\r\n")
-            .collect::<Vec<_>>();
-        let first_byte = splitted
-            .first()
-            .ok_or("args length is zero")?
-            .chars()
-            .nth(0)
-            .ok_or("first byte cannot be found")?;
-        if first_byte != '*' {
-            return Err("input is not redis Array type");
-        }
-        // now we have: *2 $4 ECHO $3 hey ""
-        let array_len = splitted[0][1..].parse::<usize>().unwrap();
-        if splitted.len() != array_len * 2 + 2 {
-            return Err("array length is does not match content");
-        }
-        // trim the last unused "" because of splitting
-        Ok(Array(splitted[..splitted.len() - 1].to_vec()))
-    }
-    fn content(&self) -> Vec<&'a str> {
-        // skip the first item because it is redis Array's length metadata
-        self.0[1..].to_vec()
-    }
-}
 
 struct Null;
 impl RespIO for Null {
