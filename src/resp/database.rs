87ca4eec227b765bb9f0b0b4828b0effe172e7f5
Evan Arlian <evanarlian2000@gmail.com>
2024-02-03 17:05:28 +0700

    internal db all done

--- src/resp/database.rs
+++ src/resp/database.rs
@@ -1,3 +1,4 @@
+// TODO this should not be coupled with resp, yank out this file later
 use std::{
     collections::HashMap,
     sync::{Arc, RwLock},
@@ -11,7 +12,6 @@ pub struct RedisValue {
     pub expiry: Option<SystemTime>,
 }
 
-// TODO should i encalpusalte this??? user does not need to know this
 struct Wrapper {
     r: RedisValue,
     i: usize,
@@ -20,7 +20,6 @@ struct Wrapper {
 pub type Database = Arc<RwLock<HashMap<String, RedisValue>>>;
 
 // https://interviewing.io/questions/insert-delete-get-random-o-1
-// this should not be coupled with resp, yank out this file later
 struct RandomMap {
     map: HashMap<String, Wrapper>,
     vec: Vec<String>,
@@ -42,16 +41,61 @@ impl RandomMap {
         );
         self.map.len()
     }
-    pub fn del(&mut self, key: &str) -> bool {
-        // INTERNAL FUNCTION: THE D of CRUD
-        // will always always remove the key
-        // returns true if deletion happens because of caller intention
-        // returns false if either key does not exist, or because of expired
-        todo!()
+    fn reorganize_map_vec(&mut self, del_i: usize) {
+        // because of o(1) random access, need to maintain both map and vec
+        // 1. swap vec[del_i] with the last element
+        let last_idx = self.vec.len() - 1;
+        self.vec.swap(del_i, last_idx);
+        // 2. pop last elem, guaranteed success if everything is right
+        let last_key = self.vec.pop().unwrap();
+        // 3. change the map[last_key] to point to originally deleted location
+        self.map.get_mut(&last_key).unwrap().i = del_i;
     }
     pub fn evict(&mut self, key: &str) {
-        // only delete on expired key
-        todo!()
+        // extract out the deleted index inside braces to satisfy borrow checker
+        let maybe_del_i = {
+            // if let below explained: all these must match if want to evict:
+            // * key exists
+            // * expiry exists
+            // * now is in the past
+            if let Some(Wrapper {
+                r:
+                    RedisValue {
+                        content: _,
+                        expiry: Some(expiry),
+                    },
+                i: del_i,
+            }) = self.map.get(key)
+            {
+                // timeline:
+                // (expiry)   (now)  ->  if expiry is in the past (elapsed exist), delete
+                // (now)   (expiry)  ->  if expiry is in the future (elapsed error)
+                if expiry.elapsed().is_ok() {
+                    Some(*del_i) // evict!
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        };
+        if let Some(del_i) = maybe_del_i {
+            self.map.remove(key);
+            self.reorganize_map_vec(del_i);
+        }
+    }
+    pub fn del(&mut self, key: &str) -> Option<RedisValue> {
+        // THE D of CRUD
+        // note that eviction has been handled by self.evict
+        // will always remove the value, and returning previous value if exists (useful for getdel)
+        self.evict(key);
+        if let Some(Wrapper { r: del_r, i: del_i }) = self.map.remove(key) {
+            self.reorganize_map_vec(del_i);
+            Some(del_r)
+        } else {
+            // key does not exist, do nothing
+            None
+        }
     }
     fn _get(&mut self, key: &str) -> Option<Wrapper> {
         // INTERNAL FUNCTION: THE R of CRUD
@@ -61,9 +105,9 @@ impl RandomMap {
     }
     pub fn set(&mut self, key: String, new_r: RedisValue) -> Option<RedisValue> {
         // THE CU of CRUD
-        // note that eviction has been handled bu self._get
+        // note that eviction has been handled by self._get
         // returns the previous value of the key, useful for cmd GETSET
-        if let Some(Wrapper { r: old_r, i: old_i }) = self._get(&key) {
+        if let Some(Wrapper { r: old_r, i: old_i }) = self._get(&key) {  // TODO insert API does return too, check later
             // replace the just that one key of the map
             let new_w = Wrapper { r: new_r, i: old_i };
             self.map.insert(key, new_w);
@@ -79,14 +123,8 @@ impl RandomMap {
             None
         }
     }
-
     pub fn get(&mut self, key: &str) -> Option<RedisValue> {
         // the sole purpose of this get is to remove internal Wrapper
         self._get(key).map(|w| w.r)
     }
-    // fn getdel(&mut self, key: &str) -> Option<RedisValue> {
-    //     self.create(key);
-    //     self.delete(key);
-    //     todo!()
-    // }
 }
